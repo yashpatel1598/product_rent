8bf0771d54ec9e9b678f14b11b825d1bce66105b
yashpatel1598 <yapatel1598@gmail.com>
2024-02-10 12:09:44 +0530

    incluse service app url

--- core/service/views.py
+++ core/service/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import *
 
 # Create your views here.
+def hello(request):
+    return  HttpResponse("Hello World")
+
+# def add(request):
+#     num1 = request.GET["num1"]
+#     num2 = request.GET["num2"]
+#     sum_result = float(num1) + float(num2)
+#     context = {"sum":sum_result}
+#     return render(request,"add/index.html",context=context)
